cd381aab6b629defdefce31af237bc6d590880a9
Menna Jaheen <menna.jaheen16@gmail.com>
2025-07-12 04:39:49 +0300

    Added pickup logic | Newspaper Screen

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour
+{
+    public GameObject playerRef;
+    public GameObject PickupRef;
+    public GameObject NewspaperUI;
+    public GameObject text;
+
+    public float triggerDistance = 4f;
+
+    bool isNewsPaperActive;
+
+    void Start()
+    {
+        text.SetActive(false);
+    }
+
+    void Update()
+    {
+        CalcDistance();
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            text.SetActive(false);
+            NewspaperUI.SetActive(true);
+            isNewsPaperActive = true;
+
+        }
+
+        if(Input.GetKeyDown(KeyCode.Escape) && isNewsPaperActive)
+        {
+            isNewsPaperActive=false;
+            NewspaperUI.SetActive(false);
+        }
+    }
+
+    void CalcDistance()
+    {
+        float distance = Vector3.Distance(playerRef.transform.position, PickupRef.transform.position);
+
+        if (distance <= triggerDistance)
+        {
+            if(!isNewsPaperActive) 
+                ApplyGlowEffectAndText();
+        }
+        else
+        {
+            text.SetActive(false);
+        }
+    }
+
+    void ApplyGlowEffectAndText()
+    {
+        text.SetActive(true);
+        // TODO: Add glow material enable here
+    }
+}

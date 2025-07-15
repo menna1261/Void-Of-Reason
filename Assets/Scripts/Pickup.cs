a61ba37318f377602bb184bca0d52f9a43418926
Menna Jaheen <menna.jaheen16@gmail.com>
2025-07-15 22:51:42 +0300

    Player can now pickup weapons and equip them

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -10,24 +10,63 @@ public class Pickup : MonoBehaviour
     public GameObject NewspaperUI;
     public GameObject text;
     public GameObject glow;
+    public WeaponManager weaponManager;
 
-    public float triggerDistance = 4f;
-
+    public float triggerDistance = 1f;
     bool isNewsPaperActive;
+
+
+    [System.Serializable]
+    public class PickupEntry
+    {
+        public string PickupName;
+        public GameObject PickupPrefab;
+    }
+ 
+    public List<PickupEntry> Pickups = new List<PickupEntry>();
+    private Dictionary<string , GameObject> PickDict = new Dictionary<string, GameObject>();
+
+
 
     void Start()
     {
         text.SetActive(false);
+        
+        foreach (PickupEntry entry in Pickups)
+        {
+            if (!PickDict.ContainsKey(entry.PickupName))
+            {
+                PickDict.Add(entry.PickupName, entry.PickupPrefab);
+            }
+        }
+
+        foreach (var key in PickDict.Keys)
+        {
+            Debug.Log($"--{key}");
+        }
     }
 
     void Update()
     {
-        CalcDistance();
+       string CurrentPickup =  CalcDistance();
         if (Input.GetKeyDown(KeyCode.P))
         {
             text.SetActive(false);
-            NewspaperUI.SetActive(true);
-            isNewsPaperActive = true;
+
+            if(CurrentPickup == "NewsPaper")
+            {
+                NewspaperUI.SetActive(true);
+                isNewsPaperActive = true;
+
+            }
+            else
+            {
+                PickDict[CurrentPickup].SetActive(false);
+                weaponManager.EquipWeapon(CurrentPickup);
+
+
+            }
+
 
         }
 
@@ -38,20 +77,27 @@ public class Pickup : MonoBehaviour
         }
     }
 
-    void CalcDistance()
-    {
-        float distance = Vector3.Distance(playerRef.transform.position, PickupRef.transform.position);
 
-        if (distance <= triggerDistance)
+    //Returns the name of the closest pickup
+    string CalcDistance()
+    {
+        foreach (PickupEntry entry in Pickups)
         {
-            if(!isNewsPaperActive) 
-                ApplyGlowEffectAndText();
+            float Distance = Vector3.Distance(playerRef.transform.position, entry.PickupPrefab.transform.position);
+            if (Distance <= triggerDistance)
+            {
+                if (!isNewsPaperActive)
+                    ApplyGlowEffectAndText();
+                return entry.PickupName;
+            }
+            else
+            {
+                text.SetActive(false);
+                glow.SetActive(false);
+            }
         }
-        else
-        {
-            text.SetActive(false);
-            glow.SetActive(false);
-        }
+
+        return "";
     }
 
     void ApplyGlowEffectAndText()
